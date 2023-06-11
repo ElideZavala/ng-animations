590461e2939cdfc0aaaeaabcd9351384b656d120
Elide <zavalavinagreelide@gmail.com>
2023-06-11 14:57:59 -0600

    renderer2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   isFavorite = false;
-  showBoring = false;
   imageUrl = 'https://i.imgur.com/C99E0zT.png';
+
+  constructor(private renderer: Renderer2) { }
 
   couseGoals = [
     { title: 'Master Angular Styling', isActiveGoal: true },
     { title: 'Understand Angular Animations', isActiveGoal: false },
     { title: 'Master Angular Animations', isActiveGoal: false }
   ]
+
+  onShowBoring(element: HTMLElement) {
+    this.renderer.setStyle(element, 'display', 'block');
+  }
 }
